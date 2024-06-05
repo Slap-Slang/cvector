9abe21711e0142522bee34ca554750a1bbe05631
Slap-Slang <charlvw23@gmail.com>
2024-06-05 21:56:18 +0200

    Eerste commit

---
+++ src/cvector.rs
@@ -0,0 +1,84 @@
+pub trait TooF64 {
+    fn too_f64(&self) -> f64;
+}
+
+impl TooF64 for u32 {
+    fn too_f64(&self) -> f64 {
+        *self as f64
+    }
+}
+
+impl TooF64 for f32 {
+    fn too_f64(&self) -> f64 {
+        *self as f64
+    }
+}
+
+impl TooF64 for f64 {
+    fn too_f64(&self) -> f64 {
+        *self
+    }
+}
+pub struct CVector {
+    x: f64,
+    y: f64,
+}
+
+impl CVector {
+    pub fn new<T>(x: T, y: T) -> CVector
+    where
+        T: PartialOrd + Copy + TooF64,
+    {
+        let x = x.too_f64();
+        let y = y.too_f64();
+
+        CVector { x, y }
+    }
+
+    pub fn x(&self) -> f64 {
+        self.x
+    }
+
+    pub fn y(&self) -> f64 {
+        self.y
+    }
+
+    pub fn mag(&self) -> f64 {
+        ((self.x * self.x) + (self.y * self.y)).sqrt()
+    }
+
+    pub fn add(&mut self, other: &CVector) {
+        self.x += other.x();
+        self.y += other.y();
+    }
+
+    pub fn sub(&mut self, other: &CVector) {
+        self.x -= other.x();
+        self.x -= other.y();
+    }
+
+    pub fn normalise(&mut self) {
+        let mag = self.mag();
+        self.x /= mag;
+        self.y /= mag;
+    }
+
+    pub fn mult_mag<T>(&mut self, multiple: T)
+    where
+        T: PartialOrd + Copy + TooF64,
+    {
+        let multiple = multiple.too_f64();
+
+        self.x *= multiple;
+        self.y *= multiple;
+    }
+
+    pub fn set_mag<T>(&mut self, new_mag: T)
+    where
+        T: PartialOrd + Copy + TooF64,
+    {
+        let new_mag = new_mag.too_f64();
+        self.normalise();
+        self.mult_mag(new_mag);
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,38 @@
+mod cvector;
+
+#[cfg(test)]
+mod tests {
+    use crate::cvector::CVector;
+
+    #[test]
+    fn scaling_test() {
+        let mut test_vec = CVector::new(100, 150);
+
+        assert_eq!(
+            test_vec.mag(),
+            ((100.0_f64 * 100.0) + (150.0 * 150.0)).sqrt()
+        );
+
+        test_vec.normalise();
+        assert_eq!(test_vec.mag(), 1.0_f64);
+
+        test_vec.set_mag(10);
+        let expected_mag = 10.0;
+        let epsilon = 0.000001;
+        assert!(f64::abs(test_vec.mag() - expected_mag) < epsilon);
+
+        test_vec.mult_mag(3.3);
+        let expected_mag = 33.0;
+        assert!(f64::abs(test_vec.mag() - expected_mag) < epsilon);
+    }
+
+    #[test]
+    fn arithmetic_test() {
+        let mut test_vec0 = CVector::new(10, 5);
+        let test_vec1 = CVector::new(20, 10);
+
+        test_vec0.add(&test_vec1);
+        assert_eq!(test_vec0.x(), 30.0);
+        assert_eq!(test_vec0.y(), 15.0);
+    }
+}
